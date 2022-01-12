b4b104aace31062dc0e4b0df77d22bdc5942c742
Mathias Naess Iversen <mathias.n.iversen@gmail.com>
2022-01-12 17:38:06 +0100

    Add json fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, {useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.37747&lon=5.38713&altitude=643'
+    )
+    .then(response => response.json())
+    .then(result => setData(result))
+    }
+
+  useEffect(()=>{
+    getData()
+    },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.toString()}
+          {data && data.length>0 && data.map((item)=><p>{item}</p>)}
         </p>
         <a
           className="App-link"
