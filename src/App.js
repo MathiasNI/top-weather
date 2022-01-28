23224acbc0b7d570df39d6b6f5f47a17f97f9ffc
Mathias Naess Iversen <mathias.n.iversen@gmail.com>
2022-01-28 16:14:10 +0100

    Move App content to Home

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import styled from 'styled-components';
-
-// Persistence/Locations
-import { Byfjellene } from './persistence';
-
-// Components
-import { Header } from './components';
-import { Forecast } from './components';
-import { Footer } from './components';
-
-const List = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0.4rem;
-  max-width: 60rem;
-  margin: auto;
-`;
+// Pages
+import { Home } from './pages';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <List>
-      {Byfjellene && Byfjellene.map((location, index) => (
-        <div key={index}>
-          {Forecast(location.name, location.latitude, location.longitude, location.altitude)}
-        </div>
-      ))}
-      </List>
-      <Footer/>
-    </>
+    <Home/>
   );
 }
 
