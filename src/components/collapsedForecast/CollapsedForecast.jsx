81bf923db5e03cbcfeca84cad891a663e79942b3
Mathias Naess Iversen <mathias.n.iversen@gmail.com>
2022-02-09 13:21:06 +0100

    Add daily forecast to collapsed

--- src/components/collapsedForecast/CollapsedForecast.jsx
+++ src/components/collapsedForecast/CollapsedForecast.jsx
@@ -17,18 +17,26 @@ const CollapsedForecast= (name, weatherDataList) => {
   return (
     <S.Row>
       <S.Location> {name} </S.Location>
-      {weatherDataList.properties.timeseries && weatherDataList.properties.timeseries.slice(2, 5).map((forecast, index) => (
+      {weatherDataList.properties.timeseries && weatherDataList.properties.timeseries.slice(2, 3).map((forecast, index) => (
         <S.Forecast key={index}>
-          <S.Time>
-            {forecast.time.slice(11,13)}
-          </S.Time>
           <S.Row>
-            <S.Symbol src={svgs[getSymbolIndex(forecast.data.next_1_hours.summary.symbol_code)]}/>
-            <S.Temperature  
-              style={{color: Math.round(forecast.data.instant.details.air_temperature) <= 0 ? "#03A9F1" : "#E42C64"}}
-            >
-              {Math.round(forecast.data.instant.details.air_temperature)}°
+          <S.SymbolDescription> Vær </S.SymbolDescription>
+          <S.TemperatureDescription> Temp. </S.TemperatureDescription>
+          <S.PrecipitationDescription> Nedbør </S.PrecipitationDescription>
+          </S.Row>
+          <S.Row>
+            <S.Symbol src={svgs[getSymbolIndex(forecast.data.next_6_hours.summary.symbol_code)]}/>
+            <S.Temperature>
+              <S.TemperatureSpan style={{ color: Math.round(forecast.data.next_6_hours.details.air_temperature_max) <= 0 ? "#03A9F1" : "#E42C64"}}>
+                {Math.round(forecast.data.next_6_hours.details.air_temperature_max)}° 
+              </S.TemperatureSpan>/ 
+              <S.TemperatureSpan style={{ color: Math.round(forecast.data.next_6_hours.details.air_temperature_min) <= 0 ? "#03A9F1" : "#E42C64"}}>
+                {Math.round(forecast.data.next_6_hours.details.air_temperature_min)}° 
+              </S.TemperatureSpan>
             </S.Temperature>
+            <S.Precipitation style={{color: forecast.data.next_6_hours.details.precipitation_amount <= 0 ? "#2F333C" : "#6EC9F0"}}>
+              {Math.round(forecast.data.next_6_hours.details.precipitation_amount)}<S.PrecipitationSpan>mm</S.PrecipitationSpan>
+            </S.Precipitation>
           </S.Row>
         </S.Forecast>
       ))}
--- src/components/collapsedForecast/styles.js
+++ src/components/collapsedForecast/styles.js
@@ -7,26 +7,64 @@ export const Location = styled.h4`
 `;
 
 export const Forecast = styled.div`
-  display: flex;
-  flex-direction: column;
   align-items: center;
-  padding: 0.2rem;
+  width: 60%;
+`;
+
+export const SymbolDescription = styled.p`
+  margin: 0.2rem 0rem 0rem 0rem;
+  text-align: center;
+  font-size: 0.7rem;
+  color: white;
   width: 20%;
 `;
 
-export const Time = styled.p`
-  margin: 0rem;
-  color: white;
-  font-size: 0.8rem;
+export const Symbol = styled.img`
+  padding-top: 0.3rem;
+  max-height: 40px;
+  width: 20%;
 `;
 
-export const Symbol = styled.img`
-  width: 2.5rem;
+export const TemperatureDescription = styled.p`
+  margin: 0.2rem 0rem 0rem 0.4rem;
+  text-align: center;
+  font-size: 0.7rem;
+  color: white;
+  width: 50%;
 `;
 
 export const Temperature = styled.p`
-  margin: 0.2rem 0rem 0rem 0.2rem;
+  margin: 0.2rem 0rem 0rem 0.4rem;
+  text-align: center;
+  font-size: 0.9rem;
+  color: #7B7E83;
+  width: 50%;
+`;
+
+export const TemperatureSpan = styled.span`
+  padding-left: 0.2rem;
+  padding-right: 0.2rem;
+`;
+
+export const PrecipitationDescription = styled.p`
+  margin: 0.2rem 0rem 0rem 0.4rem;
+  text-align: center;
+  font-size: 0.7rem;
   color: white;
+  width: 40%;
+`;
+
+export const Precipitation = styled.p`
+  margin: 0.2rem 0rem 0rem 0.4rem;
+  text-align: center;
+  font-size: 0.9rem;
+  color: white;
+  width: 40%;
+`;
+
+export const PrecipitationSpan = styled.span`
+  padding-left: 0.1rem;
+  font-size: 0.7rem;
 `;
 
 export const StatusArrow = styled.img`
